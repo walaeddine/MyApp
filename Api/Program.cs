720949887ddbd6cf6bed95157927c0a9fc696008
walaeddine <walaeddine.bourahla@hotmail.com>
2023-12-13 17:05:24 +0800

    project configuration added

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,5 +1,9 @@
-var builder = WebApplication.CreateBuilder(args);
+using Api.Extensions;
+using Microsoft.AspNetCore.HttpOverrides;
 
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.ConfigureCors();
+builder.Services.ConfigureIISIntegration();
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
@@ -8,8 +12,16 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 if (app.Environment.IsDevelopment())
     app.UseDeveloperExceptionPage();
+else
+    app.UseHsts();
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.All
+});
+app.UseCors("CorsPolicy");
 app.UseAuthorization();
 app.UseSwagger();
 app.UseSwaggerUI();
