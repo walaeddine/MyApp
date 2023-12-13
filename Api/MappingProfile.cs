68b8733149046da5f442f044d25c84eca35dfc50
walaeddine <walaeddine.bourahla@hotmail.com>
2023-12-13 21:52:13 +0800

    content negotiation added.

--- Api/MappingProfile.cs
+++ Api/MappingProfile.cs
@@ -9,7 +9,8 @@ public class MappingProfile : Profile
     public MappingProfile()
     {
         CreateMap<Company, CompanyDto>()
-            .ForCtorParam("FullAddress", opt => opt.MapFrom(x => string.Join(' ', x.Address, x.Country)));
+            .ForMember(c => c.FullAddress,
+            opt => opt.MapFrom(x => string.Join(' ', x.Address, x.Country)));
 
         CreateMap<Employee, EmployeeDto>();
     }
--- Api/Program.cs
+++ Api/Program.cs
@@ -14,7 +14,11 @@ builder.Services.ConfigureServiceManager();
 builder.Services.ConfigureSqlContext(builder.Configuration);
 builder.Services.AddAutoMapper(typeof(Program));
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(config =>
+{
+    config.RespectBrowserAcceptHeader = true;
+    config.ReturnHttpNotAcceptable = true;
+}).AddXmlDataContractSerializerFormatters();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
